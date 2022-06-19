233b21a701d09207963a8ddec59bf6f86efbea60
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-06-19 22:32:53 +0500

    🔥 Code Update 🔥

--- lib/pages/home/utils/app_bar_actions.dart
+++ lib/pages/home/utils/app_bar_actions.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:note_app_sql_database/pages/home/provider.dart';
 import 'package:note_app_sql_database/services/sql_database.dart';
@@ -11,6 +12,37 @@ class ListGridViewButton extends StatelessWidget {
     final provider = context.watch<HomeProvider>();
     return IconButton(
       splashRadius: 1,
+      icon: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        transitionBuilder: (child, anim) => RotationTransition(
+          turns: child.key == const ValueKey('icon5')
+              ? Tween<double>(begin: 0.5, end: 1).animate(anim)
+              : Tween<double>(begin: 1.5, end: 1).animate(anim),
+          child: FadeTransition(opacity: anim, child: child),
+        ),
+        child: provider.currentIndex2 == 0
+            ? const Image(
+                key: ValueKey('icon5'),
+                image: AssetImage('assets/icons/grid_view.png'),
+                height: 29,
+                width: 29,
+              )
+            : const Image(
+                key: ValueKey('icon6'),
+                image: AssetImage('assets/icons/list_view.png'),
+                height: 30,
+                width: 30,
+              ),
+      ),
+      onPressed: () {
+        provider.updateLayout();
+
+        provider.currentIndex2 = provider.currentIndex2 == 0 ? 1 : 0;
+      }
+    );
+    }
+    /* IconButton(
+      splashRadius: 1,
       icon: AnimatedIcon(
         icon: AnimatedIcons.list_view,
         progress: provider.animationController,
@@ -18,8 +50,7 @@ class ListGridViewButton extends StatelessWidget {
         size: 27,
       ),
       onPressed: () => provider.handleOnPressed(),
-    );
-  }
+    );*/
 }
 
 class DeleteButton extends StatelessWidget {
@@ -30,7 +61,38 @@ class DeleteButton extends StatelessWidget {
     final provider = context.watch<HomeProvider>();
     return IconButton(
       splashRadius: 1,
-      icon: const Icon(Icons.delete),
+      icon: Stack(
+        children: [
+          const Align(
+            alignment: Alignment.center,
+            child: Image(
+              image: AssetImage('assets/icons/trash.png'),
+              height: 28,
+              width: 30,
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              height: 15,
+              width: 15,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: CupertinoColors.systemRed,
+              ),
+              child: Text(
+                provider.notesToBeDeleted.length > 10 ? '9+' : provider.notesToBeDeleted.length.toString(),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
       onPressed: () async {
         if (provider.isLoading) return;
         await SQLDatabase.instance.deleteAll(provider.notesToBeDeleted);
