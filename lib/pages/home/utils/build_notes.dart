084d4371fdfd4f49a06efdd75e1763e9b9585d09
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-06-19 20:58:30 +0500

    🔥 Code Update 🔥

---
+++ lib/pages/home/utils/build_notes.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:note_app_sql_database/pages/home/provider.dart';
+import 'package:note_app_sql_database/pages/home/utils/build_note.dart';
+import 'package:provider/provider.dart';
+
+class BuildNotes extends StatelessWidget {
+  const BuildNotes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = context.watch<HomeProvider>();
+    return provider.isPlaying
+        ? ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.all(8.0),
+            itemCount: provider.notes.length,
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 8);
+            },
+            itemBuilder: (BuildContext context, int index) {
+              return BuildNote(index: index);
+            },
+          )
+        : MasonryGridView.count(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.all(8.0),
+            itemCount: provider.notes.length,
+            crossAxisCount: 2,
+            mainAxisSpacing: 6,
+            crossAxisSpacing: 6,
+            itemBuilder: (BuildContext context, int index) {
+              return BuildNote(index: index);
+            },
+          );
+  }
+}
